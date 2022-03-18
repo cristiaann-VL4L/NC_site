c4289212ba503c037baf17ec4e310820d8cbd465
Cristian R <crincon012@gmail.com>
2022-03-18 12:21:53 -0700

    Uncontrolled Forms + Intro to Redux

---
+++ src/redux/reducer.js
@@ -0,0 +1,16 @@
+import { CAMPSITES } from '../shared/campsites';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
+
+export const intialState = {
+    campsittes: CAMPSITES,
+    comments: COMMENTS,
+    partners: PARTNERS,
+    promotions: PROMOTIONS
+};
+
+export const Reducer = (state = intialState, action) => {
+    return state;
+};
+
