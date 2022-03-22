f392b163d381afb856f29bb31dcaa4e8c253b4ba
Cristian R <crincon012@gmail.com>
2022-03-22 16:20:07 -0700

    Combining Reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,18 @@
-import { createStore } from "redux";
-import { Reducer, intialState } from "./reducer";
+import { createStore, combineReducers } from "redux";
+import { Campsites } from "./campsites";
+import { Comments } from "./comments";
+import { Partners } from "./partners";
+import { Promotions } from "./promotions";
 
 export const ConfigureStore = () => {
-  const store = createStore(Reducer, intialState);
+  const store = createStore(
+    combineReducers({
+      campsites: Campsites,
+      comments: Comments,
+      partners: Partners,
+      promotions: Promotions,
+    })
+  );
 
   return store;
 };
