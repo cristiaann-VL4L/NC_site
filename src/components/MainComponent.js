965ae578ceba604e92e82f789e774db3d504311a
Cristian R <crincon012@gmail.com>
2022-03-25 10:58:32 -0700

    React Redux Forms Revisited

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,6 +8,7 @@ import Header from "./HeaderComponent";
 import Home from "./HomeComponent";
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
+import { actions } from "react-redux-form";
 import { addComment, fetchCampsites } from "../redux/ActionCreators";
 
 const mapStateToProps = state => {
@@ -24,6 +25,7 @@ const mapDispatchToProps = {
     addComment(campsiteId, rating, author, text),
 
   fetchCampsites: () => fetchCampsites(),
+  resetFeedbackForm: () => actions.reset("feedbackForm"),
 };
 class Main extends Component {
   componentDidMount() {
@@ -85,7 +87,13 @@ class Main extends Component {
             render={() => <About partners={this.props.partners} />}
           />
           <Route path="/directory/:campsiteId" component={CampsiteWithId} />
-          <Route exact path="/contactus" component={Contact} />
+          <Route
+            exact
+            path="/contactus"
+            render={() => (
+              <Contact resetFeedbackForm={this.props.resetFeedbackForm} />
+            )}
+          />
           <Redirect to="/home" />
         </Switch>
         <Footer />
